bb01be2039c885d1765e0c606a8d02003c53cdaf
hapsterra <75114146+hapsterra@users.noreply.github.com>
2023-01-26 13:00:35 +0100

    q

--- apk/didaktikApp/src/app/games/game1/game1.page.ts
+++ apk/didaktikApp/src/app/games/game1/game1.page.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Location} from '@angular/common';
-import { Platform, ToastController } from '@ionic/angular';
-import { Base64ToGallery, Base64ToGalleryOptions } from '@ionic-native/base64-to-gallery/ngx';
 
 @Component({
   selector: 'app-game1',
-  templateUrl: 'game1.page.html',
-  styleUrls: ['game1.page.scss'],
+  templateUrl: './game1.page.html',
+  styleUrls: ['./game1.page.scss'],
 })
 export class Game1Page implements OnInit {
-
-  constructor(private route: ActivatedRoute, private location: Location) { }
+   rows = 3;
+   columns = 3;
+   tmp:any;
+   imagenesP=["../../../assets/imgs/1.png","../../../assets/imgs/2.png","../../../assets/imgs/3.png","../../../assets/imgs/4.png","../../../assets/imgs/5.png","../../../assets/imgs/6.png","../../../assets/imgs/7.png","../../../assets/imgs/8.png","../../../assets/imgs/9.png"]
+   imagenesB=["../../../assets/imgs/blank.jpg","../../../assets/imgs/blank.jpg","../../../assets/imgs/blank.jpg","../../../assets/imgs/blank.jpg","../../../assets/imgs/blank.jpg","../../../assets/imgs/blank.jpg","../../../assets/imgs/blank.jpg","../../../assets/imgs/blank.jpg","../../../assets/imgs/blank.jpg"]
+    currTile:any;
+ otherTile:any;
+  constructor() { }
 
   ngOnInit() {
+
+ 
+
+   
   }
+  
+
+ touchMove(e:any) {
+    document.getElementById(e.target.id).style.marginLeft=e.offsetX+"px" ;
+    document.getElementById(e.target.id).style.marginTop=e.offsetY+"px";
+
+    console.log(document.getElementById(e.target.id));
+}
 
 }
